0251f635fcf85f1c4aa5ed6ef6dea879810bff4c
钟任 <zhrren@qq.com>
2016-12-02 19:15:24 +0800

    更新

--- FileWatcher/FileWatcher.cs
+++ FileWatcher/FileWatcher.cs
@@ -6,51 +6,85 @@ using System.Threading;
 
 namespace Mark.FileWatcher
 {
-    public class FileWatcher
+    public class FileWatcher: IDisposable
     {
         private Timer _timer;
         private const int TimeoutMillis = 500;
 
-        private FileSystemWatcher watcher;
-        private WatcherChangeTypes changeType;
-        private string changeFullPath;
+        private FileSystemWatcher _watcher;
+        private WatcherChangeTypes _changeType;
+        private string _changeFullPath;
+        private event EventHandler<FileSystemEventArgs> _changed;
+        private bool _immediate;
 
-        public FileWatcher(string fullpath)
+        public string Fullpath { get; private set; }
+
+        public FileWatcher(string fullpath, bool immediate = false)
         {
-            watcher = new FileSystemWatcher();
-            watcher.Filter = Path.GetFileName(fullpath);
-            watcher.Path = Path.GetDirectoryName(fullpath);
-            watcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite | NotifyFilters.FileName;
-            watcher.Changed += OnWatcherChanged;
-            watcher.Created += OnWatcherChanged;
-            watcher.Deleted += OnWatcherChanged;
-            watcher.Renamed += OnWatcherRenamed;
-            watcher.EnableRaisingEvents = true;
+            Fullpath = fullpath;
+            _immediate = immediate;
+
+            _watcher = new FileSystemWatcher();
+            _watcher.Filter = Path.GetFileName(fullpath);
+            _watcher.Path = Path.GetDirectoryName(fullpath);
+            _watcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite | NotifyFilters.FileName;
+            _watcher.Changed += OnWatcherChanged;
+            _watcher.Created += OnWatcherChanged;
+            _watcher.Deleted += OnWatcherChanged;
+            _watcher.Renamed += OnWatcherRenamed;
+            _watcher.EnableRaisingEvents = true;
             _timer = new Timer(new TimerCallback(OnTimerCallback), null, Timeout.Infinite, Timeout.Infinite);
         }
 
         void OnWatcherRenamed(object sender, RenamedEventArgs e)
         {
-            changeType = e.ChangeType;
-            changeFullPath = e.FullPath;
+            _changeType = e.ChangeType;
+            _changeFullPath = e.FullPath;
             _timer.Change(TimeoutMillis, Timeout.Infinite);
         }
 
         void OnWatcherChanged(object sender, FileSystemEventArgs e)
         {
-            changeType = e.ChangeType;
-            changeFullPath = e.FullPath;
+            _changeType = e.ChangeType;
+            _changeFullPath = e.FullPath;
             _timer.Change(TimeoutMillis, Timeout.Infinite);
         }
 
         private void OnTimerCallback(object state)
         {
-            if (Changed != null) {
-                var args = new FileSystemEventArgs(changeType, Path.GetDirectoryName(changeFullPath), Path.GetFileName(changeFullPath));
-                Changed(this, args);
+            if (_changed != null) {
+                var args = new FileSystemEventArgs(_changeType, Path.GetDirectoryName(_changeFullPath), Path.GetFileName(_changeFullPath));
+                _changed(this, args);
             }
         }
 
-        public event EventHandler<FileSystemEventArgs> Changed;
+        public void Dispose()
+        {
+            _watcher.Changed -= OnWatcherChanged;
+            _watcher.Created -= OnWatcherChanged;
+            _watcher.Deleted -= OnWatcherChanged;
+            _watcher.Renamed -= OnWatcherRenamed;
+            _watcher.Dispose();
+            _timer.Dispose();
+        }
+
+        public event EventHandler<FileSystemEventArgs> Changed
+        {
+            add
+            {
+                _changed += value;
+
+                if (_immediate)
+                {
+                    _changeType = WatcherChangeTypes.All;
+                    var args = new FileSystemEventArgs(_changeType, Path.GetDirectoryName(_changeFullPath), Path.GetFileName(_changeFullPath));
+                    _changed(this, args);
+                }
+            }
+            remove
+            {
+                _changed -= value;
+            }
+        }
     }
 }
--- FileWatcher/FileWatcherManager.cs
+++ FileWatcher/FileWatcherManager.cs
@@ -2,44 +2,61 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 namespace Mark.FileWatcher
 {
     public class FileWatcherManager
     {
-        private static Dictionary<string, List<Action<FileSystemEventArgs>>> _watcher = new Dictionary<string, List<Action<FileSystemEventArgs>>>();
+        private static Dictionary<FileWatcher, List<Action<FileSystemEventArgs>>>
+            _watcher = new Dictionary<FileWatcher, List<Action<FileSystemEventArgs>>>();
 
-        public static void Watch(string fullpath, Action<FileSystemEventArgs> action, bool immediate = false)
+        public static void Watch(string fullpath, Action<FileSystemEventArgs> callback, bool immediate = false)
         {
             fullpath = Path.GetFullPath(fullpath);
-            if (!_watcher.ContainsKey(fullpath))
+            FileWatcher watcher;
+            if (!_watcher.Keys.Select(x => x.Fullpath).Contains(fullpath))
             {
-                FileWatcher watcher = new FileWatcher(fullpath);
+                watcher = new FileWatcher(fullpath);
                 watcher.Changed += Watcher_Changed;
-                _watcher[fullpath] = new List<Action<FileSystemEventArgs>>();
+                _watcher[watcher] = new List<Action<FileSystemEventArgs>>();
             }
 
-            if (!_watcher[fullpath].Contains(action))
+            watcher = _watcher.Keys.FirstOrDefault(x => x.Fullpath == fullpath);
+            var watcherActions = _watcher[watcher];
+
+            if (!watcherActions.Contains(callback))
             {
-                _watcher[fullpath].Add(action);
+                _watcher[watcher].Add(callback);
             }
 
             if (immediate)
-                action(new FileSystemEventArgs(WatcherChangeTypes.Changed,
+                callback(new FileSystemEventArgs(WatcherChangeTypes.All,
                     Path.GetDirectoryName(fullpath),
                     Path.GetFileName(fullpath)));
         }
 
         private static void Watcher_Changed(object sender, FileSystemEventArgs e)
         {
-            if (_watcher.ContainsKey(e.FullPath))
+            var watcher = _watcher.Keys.FirstOrDefault(x => x.Fullpath == e.FullPath);
+            if (watcher != null)
             {
-                var actions = _watcher[e.FullPath];
+                var actions = _watcher[watcher];
                 actions.ForEach(x =>
                 {
                     x(e);
                 });
             }
+        }
+
+        public static void Dispose()
+        {
+            foreach(var watcherEntry in _watcher)
+            {
+                watcherEntry.Key.Changed -= Watcher_Changed;
+                watcherEntry.Key.Dispose();
+            }
+            _watcher.Clear();
         }
     }
 }
